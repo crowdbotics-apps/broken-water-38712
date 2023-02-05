95f61010ebf6bd10d4b8f5247cfc950bb22c006b
Crowdbotics <team@crowdbotics.com>
2023-02-05 04:30:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-water-38712",
+  url: "https://broken-water-38712.botics.co",
+  api: "https://broken-water-38712.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
